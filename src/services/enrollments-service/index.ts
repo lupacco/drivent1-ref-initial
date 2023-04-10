67c544fe9164a2be6de7f1ff8a76a397982197e5
lupacco <pagottolc@gmail.com>
2023-04-10 13:01:46 -0300

    feat: task 2

--- src/services/enrollments-service/index.ts
+++ src/services/enrollments-service/index.ts
@@ -8,7 +8,7 @@ import { exclude } from '@/utils/prisma-utils';
 async function getAddressFromCEP(cep: string) {
   const result = await request.get(`${process.env.VIA_CEP_API}/${cep}/json/`);
 
-  if (!result.data) {
+  if (!result.data || result.data.erro) {
     throw notFoundError();
   }
 
